e431c8dfcc45d8fd186d59550d6b6e0963890e91
svukelic <svukelic@foi.hr>
2017-04-29 11:06:32 +0200

    Initial commit

---
+++ VZwars/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VZwars.Startup))]
+namespace VZwars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
